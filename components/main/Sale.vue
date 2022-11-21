c50724854c1215daa4d67c63b4b7682c3e273324
dev.huynd <huynd@ints.vn>
2022-11-21 09:04:54 +0700

    done

--- components/main/Sale.vue
+++ components/main/Sale.vue
@@ -1,26 +1,33 @@
 <template>
-  <div class="bg-sale">
-    <div class=" border-2 text-[#FFD530] flex justify-between items-center flex-wrap">
-      <div class="">
+  <div class="relative">
+    <div class="text-[#FFD530]">
+      <div class="lt-md:h-full p-6 lt-md:p-4 flex flex-col gap-3">
         <div>
-          <p class="text-[34px] font-bold ">Khuyến mãi hiện tại</p>
+          <p class="text-[3vw] lt:text-[3vw] font-bold ">Khuyến mãi hiện tại</p>
         </div>
         <div>
-          <img src="../../public/all-new.png" />
+          <img src="../../public/all-new.png" class="lt-md:w-1/3" />
         </div>
         <div>
-          <button class="bg-btn rounded-6 py-2 px-4 outline-0 border-0 bg-yellow">Xem chi tiết
+          <!-- <button class="bg-btn rounded-6 py-2 px-4 outline-0 border-0 bg-yellow">Xem chi tiết
             <img src="../../public/arrow-right.svg" />
-          </button>
+          </button> -->
+          <div > 
+            <span class="bg-yellow-400 rounded-l-2/4 p-3"> </span>
+            <span class="bg-yellow-400 rounded-r-2/4 p-3">-></span>
+          </div>
         </div>
-        <!-- <div > 
-          <span class="bg-yellow-400 rounded-l-2/4 p-3"> </span>
-          <span class="bg-yellow-400 rounded-r-2/4 p-3">-></span>
-        </div> -->
+      </div>
+      <div class="absolute bottom-0 right-3 w-2/5 ">
+        <img src="../../public/SaleGirl.png" class="max-w-full "/>
+      </div>
+      <div class="absolute bottom-0 z--12 lt-md:w-full">
+        <img src="../../public/bg-sale.png" class="max-w-full w-full lt-md:h-[7rem]"/>
       </div>
       
     </div>
   </div>
+
 </template>
 
 <script lang="ts">
@@ -30,7 +37,7 @@
 .bg-sale {
   background: url("../../public/bg-sale.png");
 }
-
+  
 .bg-btn {
   box-shadow: 4px 8px 48px rgba(255, 255, 255, 0.25);
   backdrop-filter: blur(42px);
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,28 @@
 <template>
     <div class="w-19/20 max-w-[1200px] mx-auto font-sans">
         <Nav />
-        <!-- <div class=""> </div>
+        <div class=""> </div>
         <div class="flex md:justify-between lt-md:flex-col relative z-1">
             <Content />
             <MoneyLeft />
         </div>
         <div class="relative z-8">
             <img src="../public/message.png" class="w-[12rem] lt-md:w-[9rem] fixed bottom--6 right-0 z-99" />
-        </div> -->
+        </div>
         <!-- dùng tranform 50/50  -->
-        <!-- <div class="lt-md:hidden absolute top-[8rem] right-[40rem] z-0">
+        <div class="lt-md:hidden absolute top-[8rem] right-[40rem] z-0">
             <img src="../public/Ellipse 2.png" alt="" class="blur-[150px]">
         </div>
         <div class="relative lt-md:top-[12rem]">
             <Message />
-        </div> -->
-        <MainTable/>
-        <MainSale/>
+        </div>
+        <div class="lt-md:pt-[15rem]">
+            <MainTable />
+
+        </div>
+        <div class="pt-[10rem] lt-md:pt-[2rem]">
+            <MainSale />
+        </div>
     </div>
 
 </template>
